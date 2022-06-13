ed5d899e06cb5c544bc22cbc45c7a93b579c57b4
David Ongaro <davidongaroh@gmil.com>
2022-06-13 20:17:12 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let taskForm = document.getElementById("create-task-form");
+  
+  taskForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const taskDescription = document.getElementById("new-task-description");
+    const li = document.createElement("li");
+    li.textContent= taskDescription.value;
+    let btn= document.createElement('button')
+  
+  btn.addEventListener('click',handleDelete)
+  btn.textContent= 'x'
+
+  li.appendChild(btn)
+  
+    appendNewTask(li);
+    e.target.reset();
+    
+  })
+  
 });
+
+
+const appendNewTask = li => {
+  document.getElementById("tasks").appendChild(li);
+};
+
+function handleDelete(e){
+  e.target.parentNode.remove()}
+
+  
+
+
